d43a763194f52dec0f7861bc272844256a357455
jz.zhao <jz.zhao@signway.cn>
2021-08-12 15:38:20 +0800

    111

---
+++ test/src/main/java/com/signway/test/Test.java
@@ -0,0 +1,14 @@
+package com.signway.test;
+
+/**
+ * @author: zjz
+ * @email: jz.zhao@signway.cn
+ * @description:
+ * @date :2021/8/12 15:36
+ */
+public class Test {
+
+    public static String getHHH(String hhh){
+        return "你好！" + hhh;
+    }
+}
